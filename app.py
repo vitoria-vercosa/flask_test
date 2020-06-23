42a7bf95b1a6cb357b121b58e894e7cbf0d8c04d
vitoria-vercosa <57641867+vitoria-vercosa@users.noreply.github.com>
2020-06-23 16:00:36 -0300

    Get some others params and return a dict

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS,cross_origin
+import jsonify
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -10,10 +11,12 @@ def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
+@app.route('/<name>/<int:age>/<gender>/')
 @cross_origin()
-def hello_name(name):
-    return "Hello {}!".format(name)
+def hello_name(name, age, gender):
+    dic = {'name':name, 'age':age,'gender':gender}
+    return jsonify(dic)
+    #return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8897)
