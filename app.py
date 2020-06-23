d507899f7e0bbc4a6e0911b73c3a4c3d95788b54
vitoria-vercosa <57641867+vitoria-vercosa@users.noreply.github.com>
2020-06-23 13:04:27 -0300

    Add CORS

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS,cross_origin
 
+app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
+@cross_oringin()
 def hello():
     return "Hello World!"
 
 
 @app.route('/<name>')
+@cross_origin()
 def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5005)
